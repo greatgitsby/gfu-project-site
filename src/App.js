9dc25c152f68a2d75ce41dc233854917f63a6e38
greatgitsby <tmoen18@georgefox.edu>
2020-10-23 14:59:46 -0700

    added mission statement

--- src/App.js
+++ src/App.js
@@ -2,15 +2,33 @@ import React from 'react';
 import logo from './gfu.png';
 import './App.css';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h2>
+          GFU EPIC
+        </h2>
         <h3>
-          GFU Engineering Project Commission
+          Engineering Project Innovation Commission
         </h3>
-        <p>Contact us at gfuprojects@gmail.com</p>
+        <p>
+          We are a team of five multi-disciplinary engineering,
+          Computer Science, and Information Systems students who
+          believe that all high school students deserve a quality
+          education even during this time of comprehensive distance
+          learning. Our desire is to serve students by providing 
+          educators like you with tools and resources that enable them to
+          reach their students in meaningful ways.
+        </p>
+        <p>
+          We encourage you to fill out the Project Ideation Form if
+          you have questions or project ideas!
+        </p>
+        <h3>
+          Resources
+        </h3>
         <a
           className="App-link"
           href="https://forms.gle/WBk8XgzJiv22jfea7"
@@ -19,9 +37,11 @@ function App() {
         >
           Project Ideation Form
         </a>
+        <h5>
+          If you have any questions about us, what we are about, 
+          or the submission process, contact us at gfuprojects@gmail.com
+        </h5>
       </header>
     </div>
   );
 }
-
-export default App;
