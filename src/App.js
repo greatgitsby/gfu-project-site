8e62b45c63f1481a3a1c7f7ffa9ced71b30cb7f2
greatgitsby <tmoen18@georgefox.edu>
2021-02-05 13:36:23 -0800

    Add binary box text and image.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './gfu.png';
 import teampic from './img/team.jpg';
 import teampic_masked from './img/team-masked.jpg';
 import workflow from './img/flow.png';
+import binarybox from './img/binarybox.jpeg';
 import { Button } from '@material-ui/core';
 
 import './App.css';
@@ -18,18 +19,6 @@ export default function App() {
         <h3>
           Engineering Project Innovation Commission
         </h3>
-        <p>
-          We are a team of five multi-disciplinary engineering,
-          Computer Science, and Information Systems students whose desire
-          is to serve educators by providing tools and resources that enable
-          them to reach their students in meaningful ways.
-          We encourage you to fill out the Project Ideation Form if
-          you have questions or project ideas!
-        </p>
-
-        <h3>
-          Resources
-        </h3>
         <Button
           variant="contained"
           className="App-link"
@@ -42,8 +31,21 @@ export default function App() {
         </Button>
         <h5>
           If you have any questions about us, what we are about, 
-          or the submission process, contact us at gfuprojects@gmail.com
+          or the submission process, contact us at <strong>gfuprojects@gmail.com</strong>
         </h5>
+        <p>
+          We are a team of five multi-disciplinary engineering,
+          Computer Science, and Information Systems students whose desire
+          is to serve educators by providing tools and resources that enable
+          them to reach their students in meaningful ways.
+          We encourage you to fill out the Project Ideation Form if
+          you have questions or project ideas!
+        </p>
+
+        <img class="App-pic" src={binarybox} alt="binary box pic" />
+        <p>
+          As an example, we are currently building a small learning device designed to help students understand binary numbers. The client had voiced the difficulty in teaching his students about binary numbers. Our team took his struggle and worked to develop a solution. We completed the brainstorming and early design phases and are finishing the final product; the client will be receiving his complete product soon!
+        </p>
 
         <img src={workflow} className="App-workflow" alt="workflow" />
 
